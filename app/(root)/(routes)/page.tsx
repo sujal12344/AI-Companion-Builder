3713028f9d812646ea76c027c1498a8d6ea2073f
Sujal <sujalkesharwani220@gmail.com>
2024-08-01 20:21:48 +0530

    formater

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -3,9 +3,10 @@ import React from "react";
 
 const page = () => {
   return (
-    <div className="flex justify-center items-center h-[70vh] scale-[2]">
-      <UserButton afterSignOutUrl="/" />
-    </div>
+    // <div className="flex justify-center items-center h-[70vh] scale-[2]">
+    //   <UserButton afterSignOutUrl="/" />
+    // </div>
+    <div className=""></div>
   );
 };
 
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,14 @@
 import Navbar from "@/components/Navbar";
-import React from "react";
+import Sidebar from "@/components/Sidebar";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full">
       <Navbar />
-      <main>{children}</main>
+      <div className=" md:flex mt-16 w-20 flex-col fixed inset-y-0">
+        <Sidebar />
+      </div>
+      <main className="md:pl-20 pt-16 h-full">{children}</main>
     </div>
   );
 };
