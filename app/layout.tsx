ffd97f08ec1417b196dd0337d570a88d197195db
Sujal <sujalkesharwani220@gmail.com>
2024-08-02 23:36:03 +0530

    formatter

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/theme-provider";
+import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +21,7 @@ export default function RootLayout({
     <ClerkProvider>
       {/* <html lang="en" suppressHydrationWarning> */}
       <html lang="en">
-        <body className={inter.className}>
+        <body className={cn(`bg-secondary`, inter.className)}>
           <ThemeProvider
             attribute="class"
             defaultTheme="system"
