95771e6bacc6ea977160be24bff7fe69b3c021ca
Sujal <sujalkesharwani220@gmail.com>
2024-08-08 20:37:21 +0530

    ChatMessage Added

---
+++ components/ChatMessage.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { BeatLoader } from "react-spinners";
+import { cn } from "@/lib/utils";
+
+import { Copy } from "lucide-react";
+import { useTheme } from "next-themes";
+import { useToast } from "@/components/ui/use-toast";
+
+import { BotAvatar } from "@/components/BotAvatar";
+import { UserAvatar } from "@/components/UserAvatar";
+import { Button } from "@/components/ui/button";
+
+export interface ChatMessageProps {
+  role: "system" | "user";
+  content?: string;
+  isLoading?: boolean;
+  src?: string;
+}
+
+export const ChatMessage = ({
+  role,
+  content,
+  isLoading,
+  src,
+}: ChatMessageProps) => {
+  const { toast } = useToast();
+  const { theme } = useTheme();
+
+  const onCopy = () => {
+    if (!content) {
+      return;
+    }
+    navigator.clipboard.writeText(content);
+    toast({
+      description: "Message copied to clipboard",
+    });
+  };
+  return (
+    <div
+      className={cn(
+        "group flex items-start gap-x-3 py-4 w-full",
+        role === "user" && "justify-end"
+      )}
+    >
+      {role !== "user" && src && <BotAvatar src={src!} />}
+      <div className="bg-primary/20 px-4 py-2 rounded-md max-w-sm text-sm">
+        {isLoading ? (
+          <BeatLoader
+            color={theme === "light" ? "dark" : "white"}
+            loading={isLoading}
+            size={5}
+          />
+        ) : (
+          content
+        )}
+      </div>
+      {role === "user" && <UserAvatar />}
+      {role !== "user" && !isLoading && (
+        <Button
+          className="opacity-0 group-hover:opacity-100 transition"
+          size={"icon"}
+          variant={"ghost"}
+          onClick={onCopy}
+        >
+          <Copy className="w-4 h-4" />
+        </Button>
+      )}
+    </div>
+  );
+};
