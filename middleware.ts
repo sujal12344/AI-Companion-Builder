35e9da9679da55430e54a147636b11bab0ecdf95
Sujal <sujalkesharwani220@gmail.com>
2024-10-21 01:48:11 +0530

    middleware changes

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,12 @@
-import { authMiddleware, clerkMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs"
 
-// export default clerkMiddleware();
-export default authMiddleware();
+export default authMiddleware({
+  publicRoutes: ['/','/api/webhook'],
+})
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    // "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/((?!.*\\..*|_next).*)",
-    "/", // Always run for the root route
-    // Always run for API routes
-    "/(api|trpc)(.*)",
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
   ],
-};
+}
