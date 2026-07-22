b54375f00deaf7c0301d8b660d40db1c45028549
sujal <sujalkesharwani220@gmail.com>
2026-07-22 18:58:50 +0530

    chore: downgraded prisma version

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,11 +1,10 @@
 import { PrismaClient } from "@prisma/client";
-import config from "@/prisma.config";
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-const prismadb = globalThis.prisma || new PrismaClient(config);
+const prismadb = globalThis.prisma || new PrismaClient();
 if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb;
 
 export default prismadb;
